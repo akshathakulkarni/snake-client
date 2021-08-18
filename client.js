54522c474ca83489b0fbe563361aecf43bef270f
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-08-18 00:49:52 -0400

    Added Snake initials

--- client.js
+++ client.js
@@ -4,21 +4,24 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({ //creates a connection with the server by connecting to its IP address and specified port.
     host: 'localhost',
     port: 50541
   });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8'); // interpret incoming data as text
   conn.on('connect', () => {
-    conn.write('Hello from client!');
-  });
-  conn.on('data', (data) => {
-    console.log(`Server said : ${data}`);
+    console.log(`Successfully connected to game server!`);
   })
+  conn.on('connect', () => { // Sending data to server
+    conn.write('Hello from client!');
+    conn.write('Name: AAK');
+  });
+  conn.on('data', (data) => { // Receiving data from server.
+    console.log(`Server said : ${data}`);
+  });
   return conn;
 }
 
-module.exports = {
+module.exports = { //Exporting an object containing the connect function and hence the use of {}.
   connect
 };
--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-const { connect } = require('./client');
+const { connect } = require('./client'); //Importing an object 'connect' from client and hence the use of {}.
 
 console.log('Connecting ...');
 connect();
