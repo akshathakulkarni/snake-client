7cb091b79d4a198bd37ee6f7977f5423dd787925
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-08-17 23:58:53 -0400

    Created a connection module for client

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('connect', () => {
-    conn.write('Hello from client!');
-  });
-  conn.on('data', (data) => {
-    console.log(`Server said : ${data}`);
-  })
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
